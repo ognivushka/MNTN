e4949d1795efdfd9d74b6b3add35351b68734ef2
ognivushka <v.markevichhh@gmail.com>
2024-08-19 15:47:31 +0200

    steles for header section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-
 import Header from './components/Header.vue'
 </script>
 
 <template>
-  
-  <Header/>
+  <header class="px-20 pt-16 pb-48 bg-gray-500 font-sans  text-white">
+    <Header/>
+  </header>
  
 
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-
-
 </script>
 
 <template>
-<div class="header bg-gray-500">
-    <div class="logo">
+<div class="header flex justify-between">
+    <a href="#" class="logo">
         <img src="/src/components/icons/Logo.svg" alt="logo">
-    </div>
+    </a>
     <div class="nav cursor-pointer">
-        <ul>
+        <ul class="flex gap-10">
             <li>Equipment</li>
             <li>About us</li>
             <li>Blog</li>
         </ul>
     </div>
-    <a href="#" class="login">
+    <a href="#" class="login flex gap-2">
         <img src="/src/components/icons/cart.png" alt="account image" class="login">
         <p class="account">Account</p>
     </a>
