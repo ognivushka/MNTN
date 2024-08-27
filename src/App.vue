3ce18a21cd4c8d14706f697a0df5ea45d98f2f63
ognivushka <v.markevichhh@gmail.com>
2024-08-27 15:58:13 +0200

    hero section complited and activated

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import Header from './components/Header.vue'
-import Hero from './components/Hero.vue'
+import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+import anime from "animejs";
 </script>
 
 <template>
   <div class="background">
     <!-- Header -->
     <header class="px-20 pt-16 pb-48">
-      <Header/>
+      <Header />
     </header>
-   <!-- Hero Section -->
-    <Hero/>
+    <!-- Hero Section -->
+    <Hero />
   </div>
 </template>
 
 <style scoped>
-
-*{
+* {
   font-family: Mont, sans-serif;
   font-size: 18px;
   letter-spacing: 0.025em;
@@ -24,12 +24,11 @@ import Hero from './components/Hero.vue'
   /*background-color: #0B1D26;*/
 }
 
-.background{
-  background-image: url('/public/sky-1.png');
+.background {
+  background-image: url("/public/sky-1.png");
   background-size: cover;
   background-position: top;
   background-repeat: no-repeat;
   height: 100vh;
-  
 }
 </style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,64 @@
+<script setup lang="ts">
+import { defineProps, ref } from "vue";
+import anime from "animejs";
+
+interface Slide {
+  subtitle: string;
+  title: string;
+  active: boolean;
+}
+
+const slidesData = ref<Slide[]>([
+  {
+    subtitle: "A Hiking guide",
+    title: "Be prepared for the Mountains and beyond!",
+    active: true,
+  },
+  {
+    subtitle: "A Hiking guide 2",
+    title: "Information on the best trails and routes",
+    active: false,
+  },
+  {
+    subtitle: "A Hiking guide 3",
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    active: false,
+  },
+  {
+    subtitle: "A Hiking guide 4",
+    title: "Lconsectetur adipiscing elit Lorem ipsum dolor sit amet",
+    active: false,
+  },
+]);
+
+const showSlide = (index: number) => {
+  slidesData.value.forEach((slide, i) => {
+    slide.active = i === index;
+    if (i === index) {
+      anime({
+        targets: `.hero-slide-${i + 1}`,
+        keyframes: [{ translateY: -100 }, { translateY: 0 }],
+        opacity: [0, 1],
+        duration: 1000,
+        easing: "easeInOutSine",
+      });
+      return;
+    }
+  });
+};
+</script>
+
 <template>
   <div class="hero px-20 flex gap-80 justify-end items-center">
     <div class="hero-slider flex flex-col gap-8">
-      <div
-        v-for="(slide, index) in slidesData"
-        :key="index"
-        v-show="slide.active"
-        :class="[
-          `hero-slide-${index + 1}`,
-          'max-w-4xl',
-          'flex',
-          'flex-col',
-          'gap-8',
-          'overflow-hidden',
-        ]"
-      >
+      <div v-for="(slide, index) in slidesData" :key="index" v-show="slide.active" :class="[
+        `hero-slide-${index + 1}`,
+        'max-w-4xl',
+        'flex',
+        'flex-col',
+        'gap-8',
+        'overflow-hidden',
+      ]">
         <div class="flex gap-3 items-center">
           <div class="w-16 border-t border-accent"></div>
           <p class="uppercase text-accent tracking-[6px]">{{ slide.subtitle }}</p>
@@ -30,39 +75,12 @@
       </a>
     </div>
     <div class="hero_slider-navigation flex flex-col items-end">
-      <a href="#" class="pr-6 pb-5 pt-5 border-r">start</a>
-      <a href="#" class="pr-6 pb-5 pt-5 border-r">01</a>
-      <a href="#" class="pr-6 pb-5 pt-5 border-r">02</a>
-      <a href="#" class="pr-6 pb-5 pt-5 border-r">03</a>
-      <a href="#" class="pr-6 pb-5 pt-5 border-r hidden">04</a>
+      <div v-for="(slide, index) in slidesData" :key="index">
+        <a href="#" :class="['pr-6', 'pb-5', 'pt-5', 'border-r', 'block']" @click="showSlide(index)">{{ "0" + (index +
+          1) }}</a>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { defineProps } from "vue";
-
-defineProps({
-  slidesData: {
-    type: Array,
-    required: true,
-    default: () => [
-      {
-        subtitle: "A Hiking guide",
-        title: "Be prepared for the Mountains and beyond!",
-        active: true,
-      },
-      {
-        subtitle: "A Hiking guide 2",
-        title: "Be prepared for the Mountains and beyond!",
-        active: false,
-      },
-      {
-        subtitle: "A Hiking guide 3",
-        title: "Be prepared for the Mountains and beyond!",
-        active: false,
-      },
-    ],
-  },
-});
-</script>
+<style scoped lang="scss"></style>
