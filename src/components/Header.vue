2da84ad28bcd1ce3cbaf35280da6828d20ac4e6f
ognivushka <v.markevichhh@gmail.com>
2024-08-28 14:53:24 +0200

    adding animation in header menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="header flex justify-between">
     <a href="#" class="logo">
-      <img src="/src/components/icons/Logo.svg" alt="logo" />
+      <img src="/src/components/icons/Logo.svg" alt="logo" class="hover:animate-bounce" />
     </a>
     <div class="cursor-pointer">
       <ul class="flex gap-10">
-        <li>Equipment</li>
-        <li>About us</li>
-        <li>Blog</li>
+        <li class="hover:animate-bounce">Equipment</li>
+        <li class="hover:animate-bounce">About us</li>
+        <li class="hover:animate-bounce">Blog</li>
       </ul>
     </div>
-    <a href="#" class="login flex gap-2">
+    <a href="#" class="login flex gap-2 hover:animate-bounce">
       <img src="/src/components/icons/cart.png" alt="account image" class="login" />
       <p class="account">Account</p>
     </a>
